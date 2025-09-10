ae7278e0032a2c56479d9997d7491a26b03dc167
McFly <matthieu.roman2002@gmail.com>
2025-09-10 16:25:51 +0200

    try wistia vidoe fix

--- src/components/VslVideo.tsx
+++ src/components/VslVideo.tsx
@@ -3,8 +3,7 @@
 export default function VslVideo() {
 	return (
 		<div className="w-[95%] mx-auto lg:w-3/4 lg:mx-auto relative z-20 mb-8 lg:mb-0">
-
-	<div className="wistia_responsive_padding" style={{padding: "56.25% 0 0 0", position: "relative"}}>
+			<div className="wistia_responsive_padding" style={{padding: "56.25% 0 0 0", position: "relative"}}>
 				<div className="wistia_responsive_wrapper" style={{height: "100%", left: 0, position: "absolute", top: 0, width: "100%"}}>
 					<iframe
 						src="https://fast.wistia.net/embed/iframe/70ntres3gl?web_component=true&seo=true"
@@ -22,3 +21,4 @@ export default function VslVideo() {
 			<script src="https://fast.wistia.net/player.js" async></script>
 		</div>
 	)
+}
