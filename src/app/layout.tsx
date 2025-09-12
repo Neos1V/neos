fb0b28a65049555ef8e1654d14bd8113493cd9a1
McFly <matthieu.roman2002@gmail.com>
2025-09-12 10:03:56 +0200

    d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const manropeFont = Manrope({
   variable: "--font-manrope",
@@ -21,7 +22,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${manropeFont.variable} overflow-x-hidden`}>
         {children}
-      </body>
+				<Analytics mode="production" />;
+			</body>
     </html>
   );
 }
